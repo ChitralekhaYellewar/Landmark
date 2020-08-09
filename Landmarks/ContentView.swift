ce5704e844769475fac460d4bc58c223aa7b3acd
chitralekha-yellewar <chitralekha.yellewar@gmail.com>
2020-08-09 13:30:48 +0530

    swift UI Landmark first commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Chitralekha Yellewar on 09/08/20.
+//  Copyright © 2020 Chitralekha Yellewar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .edgesIgnoringSafeArea(.top)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
